77d53efbbd8513d0977aaeebc87659115d459563
Enrico Horn <farmboy0+git@googlemail.com>
2018-09-12 21:51:50 +0200

    data: rework the DungeonMap class

--- src/main/java/data/content/DungeonMap.java
+++ src/main/java/data/content/DungeonMap.java
@@ -1,21 +1,15 @@
 package data.content;
 
 import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.util.EnumMap;
+import java.util.Map;
 
 public class DungeonMap extends DAXContent {
 	private static final int WALLS_NE_START = 0x000;
 	private static final int WALLS_SW_START = 0x100;
-	private static final int WALLS_X2_START = 0x200;
-	private static final int WALLS_X3_START = 0x300;
-
-	public static final int WALL_NORTH_CLOSE = 0;
-	public static final int WALL_EAST_CLOSE = 1;
-	public static final int WALL_SOUTH_CLOSE = 2;
-	public static final int WALL_WEST_CLOSE = 3;
-	public static final int WALL_NORTH_FAR = 5;
-	public static final int WALL_EAST_FAR = 6;
-	public static final int WALL_SOUTH_FAR = 7;
-	public static final int WALL_WEST_FAR = 8;
+	private static final int BACKDROP_START = 0x200;
+	private static final int WALLS_FLAGS_START = 0x300;
 
 	private DungeonSquare[][] map;
 
@@ -26,38 +20,106 @@ public class DungeonMap extends DAXContent {
 			throw new IllegalArgumentException("data is not a valid geo dungeon map");
 		}
 
+		data = data.slice().order(ByteOrder.LITTLE_ENDIAN);
+
 		map = new DungeonSquare[16][16];
 		for (int y = 0; y < 16; y++) {
 			int stride = y << 4;
 			for (int x = 0; x < 16; x++) {
-				int[] walls = new int[9];
-				walls[0] = (data.get(WALLS_NE_START + stride + x) & 0xF0) >> 4;
-				walls[1] = (data.get(WALLS_NE_START + stride + x) & 0x0F);
-				walls[2] = (data.get(WALLS_SW_START + stride + x) & 0xF0) >> 4;
-				walls[3] = (data.get(WALLS_SW_START + stride + x) & 0x0F);
-				walls[4] = (data.get(WALLS_X2_START + stride + x) & 0xFF);
-				walls[5] = (data.get(WALLS_X3_START + stride + x) & 0xC0) >> 6;
-				walls[6] = (data.get(WALLS_X3_START + stride + x) & 0x30) >> 4;
-				walls[7] = (data.get(WALLS_X3_START + stride + x) & 0x0C) >> 2;
-				walls[8] = (data.get(WALLS_X3_START + stride + x) & 0x03);
-				map[x][y] = new DungeonSquare(walls);
+				Map<Direction, Integer> wallTypes = new EnumMap<>(Direction.class);
+				wallTypes.put(Direction.NORTH, (data.get(WALLS_NE_START + stride + x) & 0xF0) >> 4);
+				wallTypes.put(Direction.EAST, data.get(WALLS_NE_START + stride + x) & 0x0F);
+				wallTypes.put(Direction.SOUTH, (data.get(WALLS_SW_START + stride + x) & 0xF0) >> 4);
+				wallTypes.put(Direction.WEST, data.get(WALLS_SW_START + stride + x) & 0x0F);
+				int backdrop = data.get(BACKDROP_START + stride + x) & 0xFF;
+				Map<Direction, Integer> doorFlags = new EnumMap<>(Direction.class);
+				doorFlags.put(Direction.WEST, (data.get(WALLS_FLAGS_START + stride + x) & 0xC0) >> 6);
+				doorFlags.put(Direction.SOUTH, (data.get(WALLS_FLAGS_START + stride + x) & 0x30) >> 4);
+				doorFlags.put(Direction.EAST, (data.get(WALLS_FLAGS_START + stride + x) & 0x0C) >> 2);
+				doorFlags.put(Direction.NORTH, data.get(WALLS_FLAGS_START + stride + x) & 0x03);
+				map[x][y] = new DungeonSquare(wallTypes, backdrop, doorFlags);
 			}
 		}
 	}
 
-	public int getWall(int x, int y, int type) {
-		return map[x][y].getWall(type);
+	public boolean canMove(int x, int y, Direction d) {
+		DungeonSquare square = map[x][y];
+		if (square.getWall(d) > 0 && !square.isDoor(d)) {
+			return false;
+		}
+		switch (d) {
+			case NORTH:
+				return y > 1;
+			case EAST:
+				return x < 14;
+			case SOUTH:
+				return y < 14;
+			case WEST:
+				return x > 1;
+		}
+		return false;
+	}
+
+	public int wallIndexAt(int x, int y, Direction o) {
+		return map[x][y].getWall(o);
 	}
 
 	private static class DungeonSquare {
-		private int[] walls;
+		private Map<Direction, Integer> wallTypes;
+		private Map<Direction, Integer> doorFlags;
+		private int backdrop;
 
-		private DungeonSquare(int[] walls) {
-			this.walls = walls;
+		private DungeonSquare(Map<Direction, Integer> wallTypes, int backdrop, Map<Direction, Integer> doorFlags) {
+			this.wallTypes = wallTypes;
+			this.backdrop = backdrop;
+			this.doorFlags = doorFlags;
 		}
 
-		private int getWall(int type) {
-			return walls[type];
+		private int getWall(Direction d) {
+			return wallTypes.get(d);
+		}
+
+		private int getBackdrop() {
+			return backdrop;
+		}
+
+		private boolean isDoor(Direction d) {
+			return doorFlags.get(d) > 0;
+		}
+
+		private boolean isBashableDoor(Direction d) {
+			return doorFlags.get(d) == 3;
+		}
+	}
+
+	public enum Direction {
+		NORTH(0, -1), EAST(1, 0), SOUTH(0, 1), WEST(-1, 0);
+		private int deltaX;
+		private int deltaY;
+
+		private Direction(int deltaX, int deltaY) {
+			this.deltaX = deltaX;
+			this.deltaY = deltaY;
+		}
+
+		public Direction getRight() {
+			return Direction.values()[(ordinal() + 1) % 4];
+		}
+
+		public Direction getReverse() {
+			return Direction.values()[(ordinal() + 2) % 4];
+		}
+
+		public Direction getLeft() {
+			return Direction.values()[(ordinal() + 3) % 4];
+		}
+
+		public int getDeltaX() {
+			return deltaX;
+		}
+
+		public int getDeltaY() {
+			return deltaY;
 		}
 	}
 }
