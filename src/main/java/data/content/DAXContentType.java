a58925bcf87bf84c16dde90a84e2c152f7c2b905
Enrico Horn <farmboy0+git@googlemail.com>
2018-12-15 12:44:59 +0100

    data: Extends DAXContentType for TLB support

--- src/main/java/data/content/DAXContentType.java
+++ src/main/java/data/content/DAXContentType.java
@@ -1,15 +1,15 @@
 package data.content;
 
 public enum DAXContentType {
-	_8X8D("8X8D[1-9].DAX"), //
-	BACK("BACK[1-9].DAX"), //
-	BIGPIC("BIGPIC[1-9].DAX"), //
-	ECL("ECL[1-9].DAX"), //
-	GEO("GEO[1-9].DAX"), //
-	PIC("PIC[1-9].DAX"), //
-	SPRIT("SPRIT[1-9].DAX"), //
-	TITLE("TITLE.DAX"), //
-	WALLDEF("WALLDEF[1-9].DAX");
+	_8X8D("8X8D[1-9BC]?\\.(DAX|TLB)"), //
+	BACK("BACK[1-9]?\\.(DAX|TLB)"), //
+	BIGPIC("BIGPI[CX][1-9]?\\.(DAX|TLB)"), //
+	ECL("ECL[1-9]?\\.(DAX|GLB)"), //
+	GEO("GEO[1-9]?\\.(DAX|GLB)"), //
+	PIC("PIC[1-9ABCDEF]?\\.(DAX|TLB)"), //
+	SPRIT("SPRIT[1-9]?\\.(DAX|TLB)"), //
+	TITLE("TITLE\\.(DAX|TLB)"), //
+	WALLDEF("WALLDEF[1-9]?\\.DAX");
 
 	private String filePattern;
 
