c2137b7d6ae13fcdc1763d5378335164021b17d3
Enrico Horn <farmboy0+git@googlemail.com>
2019-07-11 22:46:27 +0200

    engine: Implement LOOK and SEARCH game menu commands

--- src/main/java/engine/input/GameMenuLOOKHandler.java
+++ src/main/java/engine/input/GameMenuLOOKHandler.java
@@ -2,13 +2,29 @@ package engine.input;
 
 import engine.Engine;
 import engine.EngineInputAction;
+import engine.VirtualMachine;
+import engine.VirtualMemory;
 
 public class GameMenuLOOKHandler implements InputHandler {
 
 	@Override
 	public void handle(Engine engine, EngineInputAction action) {
-		// TODO Auto-generated method stub
+		engine.setNextTask(() -> {
+			engine.clear();
 
+			VirtualMachine vm = engine.getVirtualMachine();
+			VirtualMemory mem = engine.getMemory();
+
+			mem.setSearchFlagsTurnLookModeOn();
+			vm.startSearchLocation();
+			mem.setSearchFlagsTurnLookModeOff();
+			if (engine.isAbortCurrentThread()) {
+				return;
+			}
+			engine.updatePosition();
+			engine.clearSprite();
+			engine.setInputStandard(EngineInputAction.LOOK);
+		});
 	}
 
 }
--- src/main/java/engine/input/GameMenuSEARCHHandler.java
+++ src/main/java/engine/input/GameMenuSEARCHHandler.java
@@ -7,8 +7,7 @@ public class GameMenuSEARCHHandler implements InputHandler {
 
 	@Override
 	public void handle(Engine engine, EngineInputAction action) {
-		// TODO Auto-generated method stub
-
+		engine.getMemory().setSearchFlagsToggleSearchMode();
+		engine.setInputStandard(EngineInputAction.SEARCH);
 	}
-
 }
