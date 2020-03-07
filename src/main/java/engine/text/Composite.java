eb8f9de9962ca4d718b9c596e17f31e78f0ce16d
Enrico Horn <farmboy0+git@googlemail.com>
2020-03-07 02:10:56 +0100

    engine: Make GoldboxStringPart a GoldboxString in Composite

--- src/main/java/engine/text/Composite.java
+++ src/main/java/engine/text/Composite.java
@@ -4,34 +4,35 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
+import shared.GoldboxString;
 import shared.GoldboxStringPart;
 
 public class Composite extends GoldboxStringPart {
-	private List<GoldboxStringPart> parts = new ArrayList<>();
+	private List<GoldboxString> parts = new ArrayList<>();
 
-	Composite(GoldboxStringPart part) {
+	Composite(GoldboxString part) {
 		super(PartType.TEXT);
 		this.parts.add(part);
 	}
 
-	Composite(Collection<GoldboxStringPart> parts) {
+	Composite(Collection<? extends GoldboxString> parts) {
 		super(PartType.TEXT);
 		this.parts.addAll(parts);
 	}
 
-	Composite(Collection<GoldboxStringPart> parts, GoldboxStringPart part) {
+	Composite(Collection<? extends GoldboxString> parts, GoldboxString part) {
 		this(parts);
 		this.parts.add(part);
 	}
 
-	Composite plus(GoldboxStringPart part) {
+	Composite plus(GoldboxString part) {
 		return new Composite(parts, part);
 	}
 
 	@Override
 	public byte getChar(int index) {
 		int start = 0;
-		for (GoldboxStringPart part : parts) {
+		for (GoldboxString part : parts) {
 			if (index >= start && index < start + part.getLength()) {
 				return part.getChar(index - start);
 			}
@@ -43,7 +44,7 @@ public class Composite extends GoldboxStringPart {
 	@Override
 	public char getCharAsAscii(int index) {
 		int start = 0;
-		for (GoldboxStringPart part : parts) {
+		for (GoldboxString part : parts) {
 			if (index >= start && index < start + part.getLength()) {
 				return part.getCharAsAscii(index - start);
 			}
@@ -54,6 +55,6 @@ public class Composite extends GoldboxStringPart {
 
 	@Override
 	public int getLength() {
-		return this.parts.stream().mapToInt(GoldboxStringPart::getLength).sum();
+		return this.parts.stream().mapToInt(GoldboxString::getLength).sum();
 	}
 }
